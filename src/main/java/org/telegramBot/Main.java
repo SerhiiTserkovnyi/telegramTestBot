ad64a360a6c14b5b8753f07116ab6f504ea1eae0
Serhii <dizz413@gmail.com>
2023-12-17 00:52:35 +0200

    Installed a bot update listener that sends a "Hello" greeting message to the user who sent any message

--- src/main/java/org/telegramBot/Main.java
+++ src/main/java/org/telegramBot/Main.java
@@ -1,6 +1,9 @@
 package org.telegramBot;
 
 import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
 
 public class Main {
     private static final String CONFIG_FILE = "src\\main\\resources\\config.properties";
@@ -9,7 +12,17 @@ public class Main {
         if (botToken != null){
             TelegramBot telegramBot = new TelegramBot(botToken);
 
-
+            telegramBot.setUpdatesListener(updates -> {
+                for (Update update : updates){
+                    if (update.message() != null && update.message().from() != null){
+                        SendMessage sendMessage = new SendMessage(update.message().from().id(), "Hello");
+                        telegramBot.execute(sendMessage);
+                    }
+                }
+                return UpdatesListener.CONFIRMED_UPDATES_ALL;
+            });
+        } else {
+            System.out.println("Bot token is not specified in the config.properties file.");
         }
     }
 }
