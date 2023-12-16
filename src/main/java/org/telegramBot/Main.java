434fc63e51c62014824f8be27384e856c4249a8d
Serhii <dizz413@gmail.com>
2023-12-17 00:38:50 +0200

    Get the token

--- src/main/java/org/telegramBot/Main.java
+++ src/main/java/org/telegramBot/Main.java
@@ -1,8 +1,8 @@
 package org.telegramBot;
 
 public class Main {
-
+    private static final String CONFIG_FILE = "src\\main\\resources\\config.properties";
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String botToken = ConfigReader.getBotToken();
     }
 }
