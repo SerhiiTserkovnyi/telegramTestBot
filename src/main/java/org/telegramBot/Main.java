6f2ff52333e62c179f030259fb21884b38a53573
Serhii <dizz413@gmail.com>
2023-12-17 00:42:12 +0200

    Check if we have a token, if so, create a bot

--- src/main/java/org/telegramBot/Main.java
+++ src/main/java/org/telegramBot/Main.java
@@ -1,8 +1,15 @@
 package org.telegramBot;
 
+import com.pengrad.telegrambot.TelegramBot;
+
 public class Main {
     private static final String CONFIG_FILE = "src\\main\\resources\\config.properties";
     public static void main(String[] args) {
         String botToken = ConfigReader.getBotToken();
+        if (botToken != null){
+            TelegramBot telegramBot = new TelegramBot(botToken);
+
+
+        }
     }
 }
