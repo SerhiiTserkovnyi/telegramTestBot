132badb13d703a6b730f149e6b90c38d6509038f
Serhii <dizz413@gmail.com>
2023-12-16 23:04:19 +0200

    wrote a class that accepts a token from the settings file

--- src/main/java/org/telegramBot/ConfigReader.java
+++ src/main/java/org/telegramBot/ConfigReader.java
@@ -1,4 +1,22 @@
 package org.telegramBot;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ConfigReader {
+    private static final String CONFIG_FILE = "config.properties";
+    private static final Properties properties = new Properties();
+
+    static {
+        try (FileInputStream input = new FileInputStream(CONFIG_FILE)) {
+            properties.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getBotToken() {
+        return properties.getProperty("bot.token");
+    }
 }
